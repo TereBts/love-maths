3b00ee49d7f843c0a8e2318e57ab0ba78ae14d44
TereBts <teresamadelinebatista@gmail.com>
2025-06-24 20:21:08 +0100

    update js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-// Wait for the Dom to finish loading before running the game
-//Get the button elements and add event listeners to them 
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
@@ -16,16 +16,16 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     runGame("addition");
-})
+
+});
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-
 function runGame(gameType) {
 
-    // Generate two random numbers between 1 and 25
+    // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
@@ -55,10 +55,11 @@ function incrementWrongAnswer() {
 }
 
 function displayAdditionQuestion(operand1, operand2) {
-    document.getElementById("operand1").textContent = operand1
-    document.getElementById("operand2").textContent = operand2;
-    document.getElementById("operator").textContent = "+";
-  
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+    
 }
 
 function displaySubtractQuestion() {
@@ -66,5 +67,5 @@ function displaySubtractQuestion() {
 }
 
 function displayMultiplyQuestion() {
-
+    
 }
